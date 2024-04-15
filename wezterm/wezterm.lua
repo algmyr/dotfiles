033a8ab41589f81b8b88d341d541b3056783f3fe
Anton Älgmyr <anton@algmyr.se>
2024-04-16 01:47:42 +0200

    Restructure wezterm, disable a keybind

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,31 +1,20 @@
--- Pull in the wezterm API
+-- vim: set fdm=marker:
+
 local wezterm = require "wezterm"
 local act = wezterm.action
 
--- This will hold the configuration.
 local config = wezterm.config_builder()
 
--- This is where you actually apply your config choices
-
--- font
--- font_hinting
--- font_locator
--- font_rasterizer
--- font_rules
--- font_shaper
--- font_size
+-- Style. {{{1
 config.font = wezterm.font_with_fallback {
   "Berkeley Mono",
   "Fira Code",
   "Noto Color Emoji",
 }
 config.font_size = 14.0
-
--- color_schemes
--- colors
 config.color_scheme = "Algmyr"
-config.hide_tab_bar_if_only_one_tab = true
-config.window_decorations = "RESIZE"
+config.cursor_thickness = 1
+config.bold_brightens_ansi_colors = false
 
 local padding = 0
 config.window_padding = {
@@ -35,11 +24,12 @@ config.window_padding = {
   bottom = padding,
 }
 
-config.cursor_thickness = 1
-config.bold_brightens_ansi_colors = false
-
+-- UI. {{{1
+config.hide_tab_bar_if_only_one_tab = true
+config.window_decorations = "RESIZE"
 config.max_fps = 144
 
+-- Keybinds. {{{1
 local function act_on_all_windows(action)
   return wezterm.action_callback(
     function(window, pane)
@@ -58,10 +48,12 @@ config.keys = {
   { key = '-', mods = 'SHIFT|CTRL', action = act_on_all_windows(act.DecreaseFontSize) },
   { key = '=', mods = 'SHIFT|CTRL', action = act_on_all_windows(act.IncreaseFontSize) },
   { key = '_', mods = 'SHIFT|CTRL', action = act_on_all_windows(act.DecreaseFontSize) },
+  { key = 'Enter',  mods = 'ALT', action = act.DisableDefaultAssignment },
 }
 
+-- Config. {{{1
 config.automatically_reload_config = false
 
--- and finally, return the configuration to wezterm
+-- }}}1
 return config
 
